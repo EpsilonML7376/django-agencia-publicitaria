b89ad5821333b216a22881344cebe8be82fdbd4e
Santiago Wursten Gill <santiwgwuri@gmail.com>
2025-06-28 21:52:14 -0300

    Update models.py

--- src/agencia/models.py
+++ src/agencia/models.py
@@ -2,41 +2,104 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
-
-class NombreAbstract(models.Model):
+class TopicoPagina(models.Model):
     nombre = models.CharField(
         _('Nombre'),
         help_text=_('Nombre descriptivo'),
-        max_length=100,
+        max_length=50,
+        unique=True
     )
-
-    def save(self, *args, **kwargs):
-        self.nombre = self.nombre.upper()
-        return super().save(*args, **kwargs)
-
+    descripcion= models.CharField(
+        _('Descripción'),
+        help_text=_('Descripción del tópico de página'),
+        max_length=150,
+        blank=True,
+        null=True
+    )
     def __str__(self) -> str:
         return f'{self.nombre}'
 
     class Meta:
-        abstract = True
         ordering = ['nombre']
-
-class TopicoPagina(NombreAbstract):
-    class Meta:
         verbose_name = _('Tópico de Página')
         verbose_name_plural = _('Tópicos de Páginas')
 
 
-class Categoria(NombreAbstract):
+class Categoria(models.Model):
+    nombre = models.CharField(
+        _('Nombre'),
+        help_text=_('Nombre descriptivo'),
+        max_length=50,
+        unique=True
+    )
+    descripcion= models.CharField(
+        _('Descripción'),
+        help_text=_('Descripción de la categoría de anuncios'),
+        max_length=150,
+        blank=True,
+        null=True
+    )
+    def __str__(self):
+        return f'{self.nombre}'
+
     class Meta:
         verbose_name = _('Categoría')
         verbose_name_plural = _('Categorías')
+        ordering = ['nombre']
 
 
-class TipoAnuncio(NombreAbstract):
+class TipoAnuncio(models.Model):
+    nombre = models.CharField(
+        _('Nombre'),
+        help_text=_('Nombre descriptivo'),
+        max_length=50,
+        unique=True
+    )
+    descripcion= models.CharField(
+        _('Descripción'),
+        help_text=_('Descripción del tipo de anuncios'),
+        max_length=150,
+        blank=True,
+        null=True
+    )
+    def __str__(self):
+        return f'{self.nombre}'
+
     class Meta:
         verbose_name = _('Tipo de Anuncio')
         verbose_name_plural = _('Tipos de Anuncios')
+        ordering = ['nombre']
+
+class Campania(models.Model):
+
+    nombre = models.CharField(
+        _('Nombre'),
+        max_length=50,
+        help_text=_('Nombre de la campaña'),
+        unique=True
+    )
+    
+
+    fecha_inicio = models.DateTimeField(
+        _('Fecha de Inicio'),
+        help_text=_('Fecha y hora de inicio de campaña')
+    )
+    fecha_fin = models.DateTimeField(
+        _('Fecha de Fin'),
+        help_text=_('Fecha y hora de fin de campaña'),
+        blank=True,
+        null=True
+    )
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        verbose_name = _('Campaña')
+        verbose_name_plural = _('Campañas')
+        ordering=['nombre']
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        if self.fecha_fin and self.fecha_fin <= self.fecha_inicio:
+            raise ValidationError(_('La fecha de fin debe ser posterior a la fecha de inicio'))
 
 
 class Anuncio(models.Model):
@@ -51,8 +114,6 @@ class Anuncio(models.Model):
         help_text=_('Tipo de anuncio'),
         related_name='anuncios',
         on_delete=models.SET_NULL,
-        blank=True,
-        null=True
     )
     titulo = models.CharField(
         _('Título'),
@@ -63,14 +124,19 @@ class Anuncio(models.Model):
         _('Contenido'),
         help_text=_('Contenido del anuncio')
     )
+    campania=models.ForeignKey(
+        Campania,
+        verbose_name=_('Campaña'),
+        help_text=_('Campaña donde aparece el anuncio'),
+        related_name='anuncios',
+        on_delete=models.PROTECT
+    )
     categoria = models.ForeignKey(
         Categoria,
         verbose_name=_('Categoría'),
         help_text=_('Categoría del anuncio'),
         related_name='anuncios',
         on_delete=models.SET_NULL,
-        blank=True,
-        null=True
     )
     precio = models.DecimalField(
         _('Precio'),
@@ -87,6 +153,31 @@ class Anuncio(models.Model):
         verbose_name_plural = _('Anuncios')
         ordering = ['nombre']
 
+class PaginaWeb(models.Model):
+    url = models.URLField(
+        _('URL'),
+        help_text=_('URL de la página web')
+    )
+    nombre = models.CharField(
+        _('Nombre'),
+        max_length=100,
+        help_text=_('Nombre de la página web')
+    )
+    topico = models.ForeignKey(
+        TopicoPagina,
+        verbose_name=_('Tópico'),
+        help_text=_('Tópico de la página web'),
+        related_name='paginas',
+        on_delete=models.SET_NULL,
+    )
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = _('Página Web')
+        verbose_name_plural = _('Páginas Web')
+        ordering = ['nombre']
+
 class AparicionAnuncioPagina(models.Model):
     anuncio = models.ForeignKey(
         Anuncio,
@@ -95,11 +186,18 @@ class AparicionAnuncioPagina(models.Model):
         related_name='apariciones',
         on_delete=models.PROTECT
     )
-    fechaInicioAparicion = models.DateTimeField(
+    pagina_web=models.ForeignKey(
+        PaginaWeb,
+        verbose_name='Página Web',
+        help_text='Página web',
+        related_name='apariciones',
+        on_delete=models.PROTECT
+    )
+    fecha_inicio_aparicion = models.DateTimeField(
         _('Fecha de Inicio'),
         help_text=_('Fecha y hora de inicio de aparición')
     )
-    fechaFinAparicion = models.DateTimeField(
+    fecha_fin_aparicion = models.DateTimeField(
         _('Fecha de Fin'),
         help_text=_('Fecha y hora de fin de aparición'),
         blank=True,
@@ -119,43 +217,47 @@ class AparicionAnuncioPagina(models.Model):
         if self.fecha_fin_aparicion and self.fecha_fin_aparicion <= self.fecha_inicio_aparicion:
             raise ValidationError(_('La fecha de fin debe ser posterior a la fecha de inicio'))
 
-class PaginaWeb(models.Model):
-    url = models.URLField(
-        _('URL'),
-        help_text=_('URL de la página web')
-    )
+class Cliente(models.Model):
     nombre = models.CharField(
         _('Nombre'),
+        max_length=50,
+        help_text=_('Nombre del cliente')
+    )
+    apellido = models.CharField(
+        _('Apellido'),
+        max_length=50,
+        help_text=_('Apellido del cliente')
+    )
+    direccion_postal = models.CharField(
+        _('Dirección Postal'),
         max_length=100,
-        help_text=_('Nombre de la página web')
-    )
-    topico = models.ForeignKey(
-        TopicoPagina,
-        verbose_name=_('Tópico'),
-        help_text=_('Tópico de la página web'),
-        related_name='paginas',
-        on_delete=models.SET_NULL,
         blank=True,
-        null=True
-    )
-
-    apariciones = models.ForeignKey(
-        AparicionAnuncioPagina,
-        verbose_name=_('Aparición'),
-        help_text=_('Aparición de un anuncio en página'),
-        related_name='apariciones',
-        on_delete=models.SET_NULL,
         null=True,
-        blank=True
+        help_text=_('Dirección postal del cliente')
+    )
+    numero_telefono = models.CharField(
+        _('Número de Teléfono'),
+        max_length=30,
+        blank=True,
+        null=True,
+        help_text=_('Número de teléfono del cliente')
+    )
+    correo = models.EmailField(
+        _('Correo Electrónico'),
+        max_length=100,
+        blank=True,
+        null=True,
+        help_text=_('Correo electrónico del cliente')
     )
 
     def __str__(self):
-        return self.nombre
+        return f'{self.nombre} {self.apellido}'
 
     class Meta:
-        verbose_name = _('Página Web')
-        verbose_name_plural = _('Páginas Web')
-        ordering = ['nombre']
+        verbose_name = _('Cliente')
+        verbose_name_plural = _('Clientes')
+        ordering = ['apellido', 'nombre']
+
 
 class ContratacionAnuncio(models.Model):
     fecha_contratacion = models.DateTimeField(
@@ -166,6 +268,13 @@ class ContratacionAnuncio(models.Model):
         Anuncio,
         verbose_name=_('Anuncio'),
         help_text=_('Anuncio contratado'),
+        related_name='contrataciones',
+        on_delete=models.PROTECT
+    )
+    cliente= models.ForeignKey(
+        Cliente,
+        verbose_name=_('Cliente'),
+        help_text=_('Cliente que contrata el anuncio'),
         related_name='contrataciones',
         on_delete=models.PROTECT
     )
@@ -184,81 +293,3 @@ class ContratacionAnuncio(models.Model):
         verbose_name_plural = _('Contrataciones de Anuncios')
         ordering = ['-fecha_contratacion']
 
-class Cliente(models.Model):
-    nombre = models.CharField(
-        _('Nombre'),
-        max_length=50,
-        help_text=_('Nombre del cliente')
-    )
-    apellido = models.CharField(
-        _('Apellido'),
-        max_length=50,
-        help_text=_('Apellido del cliente')
-    )
-    direccionPostal = models.CharField(
-        _('Dirección Postal'),
-        max_length=100,
-        blank=True,
-        null=True,
-        help_text=_('Dirección postal del cliente')
-    )
-    numeroTelefono = models.CharField(
-        _('Número de Teléfono'),
-        max_length=30,
-        blank=True,
-        null=True,
-        help_text=_('Número de teléfono del cliente')
-    )
-    correo = models.EmailField(
-        _('Correo Electrónico'),
-        max_length=100,
-        blank=True,
-        null=True,
-        help_text=_('Correo electrónico del cliente')
-    )
-
-    contrataciones = models.ForeignKey(
-        ContratacionAnuncio,
-        verbose_name=_('Contratación de anuncio'),
-        help_text=_('Contrataciones de un anuncio'),
-        related_name='contrataciones',
-        on_delete=models.PROTECT
-    )
-
-    def __str__(self):
-        return f'{self.nombre} {self.apellido}'
-
-    class Meta:
-        verbose_name = _('Cliente')
-        verbose_name_plural = _('Clientes')
-        ordering = ['apellido', 'nombre']
-
-class Campania(models.Model):
-    class Meta:
-        verbose_name = _('Campaña')
-        verbose_name_plural = _('Campañas')
-
-    nombre = models.CharField(
-        _('Nombre'),
-        max_length=50,
-        help_text=_('Nombre de la campaña')
-    )
-    
-    anuncio = models.ForeignKey(
-        Anuncio,
-        verbose_name=_('Anuncio'),
-        help_text=_('Anuncio que pertenece a la campaña'),
-        related_name='campañas',
-        on_delete=models.PROTECT
-    )
-
-    fechaInicio = models.DateTimeField(
-        _('Fecha de Inicio'),
-        help_text=_('Fecha y hora de inicio de campaña')
-    )
-    fechaFin = models.DateTimeField(
-        _('Fecha de Fin'),
-        help_text=_('Fecha y hora de fin de campaña'),
-        blank=True,
-        null=True
-    )
